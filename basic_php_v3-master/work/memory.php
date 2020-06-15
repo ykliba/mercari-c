650a6d27c61b7801b39bece6506fe8d8768fc4a0
ykliba <mediaboxing2@gmail.com>
2020-06-15 20:42:12 +0900

    php

---
+++ basic_php_v3-master/work/memory.php
@@ -0,0 +1,17 @@
+<?php 
+include('_header.php');
+?>
+
+  
+    <section>
+      <img src="img/work3.png" width="400" height="260" alt="神経衰弱の紹介画像">
+      <h1>神経衰弱</h1>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <a href = "index.php">戻る</a>
+    </section>
+
+  </section>
+
+<?php 
+include('_footer.php');
+  
---
+++ basic_php_v3-master/work/treasure.php
@@ -0,0 +1,14 @@
+<?php 
+include('_header.php');
+?>
+    <section>
+      <img src="img/work2.png" width="400" height="260" alt="宝探しゲームの紹介画像">
+      <h1>宝探しゲーム</h1>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <a href = "index.php">戻る</a>
+    </section>
+  </section>
+
+<?php 
+include('_footer.php');
+  
